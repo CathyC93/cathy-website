d9f843f806c14ef341826a87b31663065872b1c0
Cathy <catherinecasey513@yahoo.com>
2019-08-29 21:18:06 -0500

    Changed paths for dropdown links

--- src/App.js
+++ src/App.js
@@ -15,23 +15,23 @@ class App extends Component {
         <header className="App-header">
         <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">
-          <img
-            src={blackLotus}
-            width="45"
-            height="45"
-            className="d-inline-block align-top"
-            alt="Black Lotus"
-          />
+            <img
+              src={blackLotus}
+              width="45"
+              height="45"
+              className="d-inline-block align-top"
+              alt="Black Lotus"
+            />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="cathy">Cathy</Nav.Link>=
               <NavDropdown title="Writing" id="basic-nav-dropdown">
-                <NavDropdown.Item href="writing/code">Code</NavDropdown.Item>
-                <NavDropdown.Item href="writing/leadership">Leadership</NavDropdown.Item>
+                <NavDropdown.Item href="code">Code</NavDropdown.Item>
+                <NavDropdown.Item href="leadership">Leadership</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="writing/blog">Blog</NavDropdown.Item>
+                <NavDropdown.Item href="blog">Blog</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
