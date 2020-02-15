cf64bcbf2ca9a8258c0a287359dc1a4733af1a52
Cathy <catherinecasey513@yahoo.com>
2020-02-15 15:50:10 -0600

    Updated coloring on navbar and changed text on landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {
+  Col,
   Figure,
   Nav,
   Navbar,
   NavDropdown,
+  Row,
 } from 'react-bootstrap'
 import React, { Component } from 'react';
 import blackLotus from './black-lotus.png';
@@ -15,40 +17,50 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">
-            <img
-              src={blackLotus}
-              width="45"
-              height="45"
-              className="d-inline-block align-top"
-              alt="Black Lotus"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="cathy">Cathy</Nav.Link>=
-              <NavDropdown title="Writing" id="basic-nav-dropdown">
-                <NavDropdown.Item href="code">Code</NavDropdown.Item>
-                <NavDropdown.Item href="leadership">Leadership</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="blog">Blog</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Figure>
-          <Figure.Image
-            width={171}
-            height={180}
-            alt="cathy"
-            src={cathy}
-          />
-          <Figure.Caption>
-            This website is currently a work in progress!
-          </Figure.Caption>
-        </Figure>
+          <Navbar expand="lg" style={{ backgroundColor: '#849264' }}>
+            <Navbar.Brand href="#home">
+              <img
+                src={blackLotus}
+                width="45"
+                height="45"
+                className="d-inline-block align-top"
+                alt="Black Lotus"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="cathy">Home</Nav.Link>
+                <NavDropdown title="Writing" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="code" style={{ fontSize: 'large' }}>Code</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="blog" style={{ fontSize: 'large' }}>Blog</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <div>
+            <Row>
+              <Col md='4'>
+                <Figure>
+                  <Figure.Image
+                    width={171}
+                    height={180}
+                    alt="cathy"
+                    src={cathy}
+                  />
+                </Figure>
+              </Col>
+              <Col md='8'>
+                <Row>
+                  Hi!  I'm Cathy.
+                </Row>
+                <Row>
+                  I read books, play games, lift weights, and write code.
+                </Row>
+              </Col>
+            </Row>
+          </div>
         </header>
       </div>
     );
