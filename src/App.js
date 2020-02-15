b2401911268bc6b8c34dfe94467ba8b7c83bd8ef
Cathy <catherinecasey513@yahoo.com>
2020-02-15 16:17:00 -0600

    Added react router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { NavBar } from './NavBar'
 import { Home } from './Home'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <NavBar />
-          <Home />
+          <Router>
+            <Route path='/' component={NavBar} />
+            <Route path='/' component={Home} />
+          </Router>
         </header>
       </div>
     );
